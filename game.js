f53b43cbe187ec97d0b673c45a651efb504f885b
hasnain <hasnainaz@googlemail.com>
2024-12-31 00:50:44 +0000

    soroban addition feature and cleaned up unused chapters

--- game.js
+++ game.js
@@ -1,9 +1,26 @@
 let question = document.getElementById('question');
 let answer_box = document.getElementById('answer');
 let score = document.getElementById('score');
-let incorrect= document.getElementById('incorrect');
+let incorrect = document.getElementById('incorrect');
 answer_box.addEventListener('keypress', input_listener);
 let selected_chapter = parseInt(sessionStorage.getItem("chapter"));
+
+let digit_slider = document.getElementById('digits');
+let size_slider = document.getElementById('size');
+
+let size = 1;
+let digits = 1;
+
+digit_slider.oninput = function() {
+    digits = this.value;
+    load_question();
+}
+size_slider.oninput = function() {
+    size = this.value;
+    load_question();
+}
+
+
 load_question();
 
 function load_question() {    
@@ -45,6 +62,9 @@ function check_answer() {
 }
 
 function load_question_1() {
+    digit_slider.remove();
+    size_slider.remove();
+
     let a = get_random(101, 9999);
     let b = get_random(101, 999);
     question.innerHTML = a + " + " + b;
@@ -52,15 +72,21 @@ function load_question_1() {
 }
 
 function load_question_2() {
+    digit_slider.remove();
+    size_slider.remove();
+
+
     let a = get_random(1000, 9999);
     let b = get_random(101, 999);
     question.innerHTML = a + " - " + b;
     answer = a - b;
- 
-
 }
 
 function load_question_3() {
+    digit_slider.remove();
+    size_slider.remove();
+
+
     let a = get_random(101, 999);
     let b = get_random(2, 9);
     question.innerHTML = a + " x " + b;
@@ -68,12 +94,20 @@ function load_question_3() {
 }
 
 function load_question_4() {
+    digit_slider.remove();
+    size_slider.remove();
+
+
     let a = get_random(11,99);
     question.innerHTML = a + "²";
     answer = a * a;
 }
 
 function load_question_5() {
+    digit_slider.remove();
+    size_slider.remove();
+
+
     let a = get_random(11, 99);
     let b = get_random(11, 99);
     question.innerHTML = a + " x " + b;
@@ -82,27 +116,49 @@ function load_question_5() {
  
 
 function load_question_6() {
+    digit_slider.remove();
+    size_slider.remove();
+
+
     let a = get_random(101, 999);
     question.innerHTML = a + "²";
     answer = a * a;
 }
 
 function load_question_7() {
+    digit_slider.remove();
+    size_slider.remove();
+
+
     let a = get_random(11, 99);
     question.innerHTML = a + "³";
     answer = a * a * a;
 }
 
-//free real estate
+//
+//  d|n
+//  1|1 - 9
+//  2|10 - 99
+//  3|100 - 999
+//  4|1000 - 9999
+//
 
 function load_question_8() {
-    let a = get_random(101, 9999);
-    let b = get_random(2, 9);
-    question.innerHTML = a + "/" + b;
-    answer = a / b;
-    console.log(answer);
-
+    let lower = Math.pow(10, digits - 1);
+    let upper = Math.pow(10, digits) - 1;
+    sums = [];
+    for(let i = 0; i < size; i++) {
+        sums.push(get_random(lower, upper));
+    }
+    let string = "";
+    sums.forEach(num => {
+        string += num.toString() + "+";
+    });
+    question.innerHTML = string.substring(0, string.length - 1);
+    answer = sums.reduce((a, b) => a + b);
 }
+
+//free real estate
 
 function load_question_9() {
     let a = get_random(11, 99);
